bd0eec91f5125f6327fd481f657bba08e7a03352
Dewald de Jager <dewald.dejager@entelect.co.za>
2021-10-14 17:20:25 +0200

    Add example requests for pulling secrets from AWS Secrets Manager

--- app.js
+++ app.js
@@ -1,41 +1,85 @@
 const { SSMClient, GetParameterCommand, GetParametersCommand } = require('@aws-sdk/client-ssm')
+const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager')
+const REGION = 'eu-west-1'
+const NAMESPACE = 'demo-api'
+const ENVIRONMENT = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 
-const client = new SSMClient({ region: 'eu-west-1' })
+const secretsManagerClient = new SecretsManagerClient({ region: REGION })
+const ssmClient = new SSMClient({ region: REGION })
 
 function testGetParameter () {
   async function getParameter (name) {
-    return await client.send(new GetParameterCommand({
-      Name: name,
+    return await ssmClient.send(new GetParameterCommand({
+      Name: `/${NAMESPACE}/${ENVIRONMENT}/${name}`,
       WithDecryption: true
     }))
   }
 
-  getParameter('/demo-api/dev/db-host').then(value => {
-    console.log(`DB Host: ${value.Parameter.Value}`)
+  getParameter('string').then(value => {
+    console.log(`String: ${JSON.stringify(value)}`)
   })
-  getParameter('/demo-api/dev/db-port').then(value => {
-    console.log(`DB Port: ${value.Parameter.Value}`)
+  getParameter('string-list').then(value => {
+    console.log(`StringList: ${JSON.stringify(value)}`)
   })
-  getParameter('/demo-api/dev/db-username').then(value => {
-    console.log(`DB Username: ${value.Parameter.Value}`)
-  })
-  getParameter('/demo-api/dev/db-password').then(value => {
-    console.log(`DB Password: ${value.Parameter.Value}`)
+  getParameter('secure-string').then(value => {
+    console.log(`SecureString: ${JSON.stringify(value)}`)
   })
 }
 
 function testGetParameters () {
   async function getParameters (names) {
-    return await client.send(new GetParametersCommand({
-      Names: names.map(name => `/demo-api/dev/${name}`),
+    return await ssmClient.send(new GetParametersCommand({
+      Names: names.map(name => `/${NAMESPACE}/${ENVIRONMENT}/${name}`),
       WithDecryption: true
     }))
   }
 
-  getParameters(['db-host', 'db-port', 'db-username', 'db-password']).then(function (response) {
-    console.log(JSON.stringify(response.Parameters.map(parameter => parameter.Value)))
+  getParameters(['string', 'string-list', 'secure-string']).then(function (response) {
+    console.log(JSON.stringify(response))
+  })
+}
+
+function testGetSecret () {
+  async function getSecret (name) {
+    return await secretsManagerClient.send(new GetSecretValueCommand({
+      SecretId: `${NAMESPACE}/${ENVIRONMENT}/${name}`
+    }))
+  }
+
+  getSecret('rds-database').then(value => {
+    console.log(`RDS: ${JSON.stringify(value)}`)
+  })
+
+  getSecret('mysql-database').then(value => {
+    console.log(`MySQL: ${JSON.stringify(value)}`)
+  })
+
+  getSecret('key-value-pairs').then(value => {
+    console.log(`Key-value pairs: ${JSON.stringify(value)}`)
+  })
+
+  getSecret('plaintext').then(value => {
+    console.log(`Plaintext: ${JSON.stringify(value)}`)
+  })
+}
+
+function testGetSecretAsParameter () {
+  async function getParameter (name) {
+    return await ssmClient.send(new GetParameterCommand({
+      Name: `/aws/reference/secretsmanager/${NAMESPACE}/${ENVIRONMENT}/${name}`,
+      WithDecryption: true
+    }))
+  }
+
+  getParameter('key-value-pairs').then(value => {
+    console.log(`Key-value pairs: ${JSON.stringify(value)}`)
+  })
+  getParameter('plaintext').then(value => {
+    console.log(`Plaintext: ${JSON.stringify(value)}`)
   })
 }
 
 testGetParameter()
 testGetParameters()
+testGetSecret()
+testGetSecretAsParameter()
